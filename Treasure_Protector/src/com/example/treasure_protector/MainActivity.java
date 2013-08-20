4e6621792ea47ef8bf39e4760ad4c97b01bc40ea
Sonjinhyuk <jhson21@naver.com>
2013-08-20 17:51:40 +0900

    App부분 여기서 하자

---
+++ Treasure_Protector/src/com/example/treasure_protector/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.treasure_protector;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+}
